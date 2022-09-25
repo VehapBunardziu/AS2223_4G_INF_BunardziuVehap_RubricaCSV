e67d17ba90b505377f802ab3f3753b912ebada84
VehapBunardziu <vehap.bunardziu@studenti.ittsrimini.edu.it>
2022-09-25 15:14:20 +0200

    Scrittura del file su TXT_File

--- AS2223_4G_INF_BunardziuVehap_RubricaCSV/FRMMain.Designer.cs
+++ AS2223_4G_INF_BunardziuVehap_RubricaCSV/FRMMain.Designer.cs
@@ -31,6 +31,12 @@
             this.label1 = new System.Windows.Forms.Label();
             this.BTN_File = new System.Windows.Forms.Button();
             this.TXT_File = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.TXT_Cognome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BTN_Visualizza = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -41,7 +47,6 @@
             this.label1.Size = new System.Drawing.Size(89, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "File CSV rubrica";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // BTN_File
             // 
@@ -49,9 +54,9 @@
             this.BTN_File.Name = "BTN_File";
             this.BTN_File.Size = new System.Drawing.Size(75, 23);
             this.BTN_File.TabIndex = 1;
-            this.BTN_File.Text = "button1";
+            this.BTN_File.Text = "...";
             this.BTN_File.UseVisualStyleBackColor = true;
-            this.BTN_File.Click += new System.EventHandler(this.button1_Click);
+            this.BTN_File.Click += new System.EventHandler(this.BTN_File_Click);
             // 
             // TXT_File
             // 
@@ -60,16 +65,69 @@
             this.TXT_File.Size = new System.Drawing.Size(540, 23);
             this.TXT_File.TabIndex = 2;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.TXT_Cognome);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(69, 87);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(633, 100);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filtri di Ricerca";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Contiene",
+            "Inizia",
+            "Finisce"});
+            this.comboBox1.Location = new System.Drawing.Point(515, 36);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(112, 23);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // TXT_Cognome
+            // 
+            this.TXT_Cognome.Location = new System.Drawing.Point(83, 36);
+            this.TXT_Cognome.Name = "TXT_Cognome";
+            this.TXT_Cognome.Size = new System.Drawing.Size(426, 23);
+            this.TXT_Cognome.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Cognome";
+            // 
+            // BTN_Visualizza
+            // 
+            this.BTN_Visualizza.Location = new System.Drawing.Point(345, 193);
+            this.BTN_Visualizza.Name = "BTN_Visualizza";
+            this.BTN_Visualizza.Size = new System.Drawing.Size(75, 23);
+            this.BTN_Visualizza.TabIndex = 4;
+            this.BTN_Visualizza.Text = "Visualizza";
+            this.BTN_Visualizza.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BTN_Visualizza);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.TXT_File);
             this.Controls.Add(this.BTN_File);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,5 +138,10 @@
         private Label label1;
         private Button BTN_File;
         private TextBox TXT_File;
+        private GroupBox groupBox1;
+        private ComboBox comboBox1;
+        private TextBox TXT_Cognome;
+        private Label label2;
+        private Button BTN_Visualizza;
     }
 }
--- AS2223_4G_INF_BunardziuVehap_RubricaCSV/FRMMain.cs
+++ AS2223_4G_INF_BunardziuVehap_RubricaCSV/FRMMain.cs
@@ -7,16 +7,13 @@ namespace AS2223_4G_INF_BunardziuVehap_RubricaCSV
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void BTN_File_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            string Temp = "E:\\Progetti Visual Studio 2022\\AS2223_4G_INF_BunardziuVehap_RubricaCSV\\AS2223_4G_INF_BunardziuVehap_RubricaCSV";
-            openFileDialog.FileName = Temp;
+            OpenFileDialog FinestraFile = new OpenFileDialog();
+            if (FinestraFile.ShowDialog() == DialogResult.OK)
+            {
+                TXT_File.Text = FinestraFile.FileName;
+            }
         }
     }
 }
